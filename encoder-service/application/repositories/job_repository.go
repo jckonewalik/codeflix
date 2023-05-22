b59370ff5185310542a53297d4a499109c4bf96b
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2023-05-22 16:59:35 -0300

    initial commit

---
+++ encoder-service/application/repositories/job_repository.go
@@ -0,0 +1,60 @@
+package repositories
+
+import (
+	"fmt"
+
+	"github.com/jckonewalik/encoder-service/domain"
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+)
+
+type JobRepository interface {
+	Insert(job *domain.Job) (*domain.Job, error)
+	Find(id string) (*domain.Job, error)
+	Update(job *domain.Job) (*domain.Job, error)
+}
+
+type JobRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func NewJobRepositoryDb(db *gorm.DB) *JobRepositoryDb {
+	return &JobRepositoryDb{Db: db}
+}
+
+func (repo *JobRepositoryDb) Insert(job *domain.Job) (*domain.Job, error) {
+	if job.ID == "" {
+		job.ID = uuid.NewV4().String()
+	}
+
+	err := repo.Db.Create(job).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+func (repo *JobRepositoryDb) Find(id string) (*domain.Job, error) {
+	var job domain.Job
+
+	repo.Db.Preload("Video").First(&job, "id = ?", id)
+
+	if job.ID == "" {
+		return nil, fmt.Errorf("job doesn't exist")
+	}
+
+	return &job, nil
+}
+
+func (repo *JobRepositoryDb) Update(job *domain.Job) (*domain.Job, error) {
+
+	err := repo.Db.Save(&job).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
